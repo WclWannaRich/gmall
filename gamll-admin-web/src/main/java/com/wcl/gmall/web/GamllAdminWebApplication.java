31478d262d47a6d7fc9fcd6a9ed3d531008e9623
weichenglong <WclWannaRich@126.com>
2020-01-11 16:40:58 +0800

    1

---
+++ gamll-admin-web/src/main/java/com/wcl/gmall/web/GamllAdminWebApplication.java
@@ -0,0 +1,13 @@
+package com.wcl.gmall.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamllAdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamllAdminWebApplication.class, args);
+    }
+
+}
---
+++ gamll-admin-web/src/test/java/com/wcl/gmall/web/GamllAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcl.gmall.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamllAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
